20abf16e899da9be7bfd4fa3ca6a833de38ad772
migsarmiento2 <Miguel.Sarmiento@theexperienceengine.com>
2018-04-16 00:00:50 +0800

    Added overlays for Licenses.

---
+++ Final Site Design/js/Overlay.js
@@ -0,0 +1,15 @@
+function showRegisterOverlay(overlayId) {
+	document.getElementById(overlayId).style.display = "block";
+};
+
+function hideRegisterOverlay(overlayId) {
+	document.getElementById(overlayId).style.display = "none";
+};
+
+function showUnregisterOverlay(overlayId) {
+	document.getElementById(overlayId).style.display = "block";
+};
+
+function hideUnregisterOverlay(overlayId) {
+	document.getElementById(overlayId).style.display = "none";
+};
